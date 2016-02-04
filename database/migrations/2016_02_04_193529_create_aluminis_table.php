6be3025beaadc2a63ba7a03d78204048b0bb53ad
Zishan Ansari <kinnngg786@gmail.com>
2016-02-05 01:09:41 +0530

    Add model and migration for Alumini

---
+++ database/migrations/2016_02_04_193529_create_aluminis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAluminisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aluminis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('speech');
+            $table->string('speaker');
+            $table->string('batch');
+            $table->string('profession');
+
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('aluminis');
+    }
+}
