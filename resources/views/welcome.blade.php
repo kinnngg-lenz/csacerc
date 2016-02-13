1f42b05c9d4f4f8bfbc277c0f989ae22405b01ae
Zishan Ansari <kinnngg786@gmail.com>
2016-02-13 17:53:02 +0530

    Not much

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
+            <div class="jumbotron">
+                <h2>Hey! {{ Auth::check() ? Auth::user()->name : "Guest" }}.</h2>
+                <h2>Welcome to Department of Computer Science - ACERC</h2>
+                <p>This Project is Under Development
+                    <a class="btn btn-primary btn-sm" target="_blank" href="https://github.com/kinnngg-lenz/csacerc" role="button">View Source</a>
+                </p>
             </div>
         </div>
     </div>
