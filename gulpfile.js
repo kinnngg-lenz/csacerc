249f230045335443377be49bfc0ea275c3df3ecc
Zishan Ansari <kinnngg786@gmail.com>
2016-02-19 04:22:34 +0530

    Change Style of Alumini Page

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ elixir(function(mix) {
     //mix.sass('app.scss');
     mix.less('app.less');
 
-    mix.styles(['bootstrap.css', 'highlight/railscasts.css', ,'bootstrap-datetimepicker.min.css', 'app.css'],null,'public/css');
+    mix.styles(['bootstrap.css', 'highlight/railscasts.css', ,'bootstrap-datetimepicker.min.css', 'bubble.css', 'app.css'],null,'public/css');
 
     mix.scripts(['jquery.min.js','bootstrap.min.js', 'modernizr.js', 'typeahead.bundle.min.js', 'masonry.pkgd.min.js', 'jquery.imagesloaded.js', 'highlight.pack.js', 'bootstrap-datetimepicker.min.js', 'jquery.countdown2.js', 'app.js'],null,'public/js');
 
--- resources/views/aluminis/index.blade.php
+++ resources/views/aluminis/index.blade.php
@@ -10,23 +10,21 @@
             </div>
             <div class="col-md-11 col-md-offset-1">
                 {{--<div class="panel panel-info text-center col-md-7 col-md-offset-2"><h3>Alumini Speak</h3></div>--}}
+
                 <div class="grid js-masonry" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 330 }'>
                 @forelse($aluminis as $alumini)
-
-                    <div class="col-sm-6 grid-item col-md-4">
-                        <div class="thumbnail">
-                            <div class="caption">
-                                <p class="well">
-                                    {!! nl2br($alumini->speech) !!}
-                                </p>
-                                <p class="blockquote-reverse">
-                                    <b> - {{ $alumini->speaker }}</b><br>
+                        <div class="col-sm-6 grid-item col-md-4">
+                        <blockquote class="example-{{ ['obtuse','right','wrong'][array_rand([0,1,2])] }}">
+                            <p>
+                                {!! nl2br(htmlentities($alumini->speech)) !!}
+                            </p>
+                        </blockquote>
+                        <p>
+                            <b> - {{ $alumini->speaker }}</b><br>
                                         <span class="text-small">
                                             {{ $alumini->batch }} <br> ({{ $alumini->profession }})</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                            </p>
+                         </div>
                 @empty
                     Empty
                 @endforelse
