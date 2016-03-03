07a1e631c9fde222d9c968ef785feb3b2abc6fe1
Zishan Ansari <kinnngg786@gmail.com>
2016-03-04 00:52:04 +0530

    Up to fort

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', $creds['MYSQL']['HOST']),
-            'database'  => env('DB_DATABASE', $creds['MYSQL']['DATABASE']),
-            'username'  => env('DB_USERNAME', $creds['MYSQL']['USER']),
-            'password'  => env('DB_PASSWORD', $creds['MYSQL']['PASSWORD']),
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
